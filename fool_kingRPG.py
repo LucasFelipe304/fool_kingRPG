e11536356e2c258a4a6e84dfcf0c079fd81e4488
Lucas Felipe <devlucasfelipe@gmail.com>
2023-06-16 21:07:58 -0300

    Informações do Guerreiro

--- fool_kingRPG.py
+++ fool_kingRPG.py
@@ -4,6 +4,10 @@ def pulaLinha():
 def linhaDivisória():
   print('|','-' * 90,'|')
 
+def informacoesPessoais():
+  print('Informações Pessoais')
+
+
 personagens = ("""I |Mago: Ewaelle
 II|Guerreiro: Vorrus
 III|Paladino: Raveneye""")
@@ -17,21 +21,46 @@ if escolhaDoPersonagem == 'I':
   pulaLinha()
   pulaLinha()
 
-def infoMago():
-  print('Informações Pessoais')
+  def infoMago():
+    informacoesPessoais()
+    pulaLinha()
+    caracteristicas = {
+      'Lugar de origem:':'Shadow Continent',
+      'Altura:': '1.76',
+      'Arma:': 'None',
+      'Arcano:':'5',
+      'Discernimento:':'4',
+      'Percepção:':'5',
+      'Medicina:':'3',
+      'Saúde:':'2',
+      'Iniciativa:':'4'
+    }
+    
+    for chave, valor in caracteristicas.items():
+      print(chave, valor)
+  infoMago()
+
+elif escolhaDoPersonagem == 'II':
+  print('Você escolheu Vorrus, o guerreiro.')
+  linhaDivisória()
   pulaLinha()
-  caracteristicas = {
-    'Lugar de origem:':'Shadow Continent',
-    'Altura:': '1.76',
-    'Arma:': 'None',
-    'Arcano:':'5',
-    'Discernimento:':'4',
-    'Percepção:':'5',
-    'Medicina:':'3',
-    'Saúde:':'2',
-    'Iniciativa:':'4'
-  }
-  
-  for chave, valor in caracteristicas.items():
-    print(chave, valor)
-infoMago()
+  pulaLinha()
+
+  def infoGuerreiro():
+    informacoesPessoais()
+    pulaLinha()
+    caracteristicas = {
+      'Lugar de origem:':'Montanhas Rústicas de Glorinhidor',
+      'Altura:': '1.83',
+      'Arma:': 'Espada',
+      'Arcano:':'-1',
+      'Discernimento:':'2',
+      'Percepção:':'3',
+      'Medicina:':'0',
+      'Saúde:':'4',
+      'Iniciativa:':'5'
+    }
+
+    for chave, valor in caracteristicas.items():
+      print(chave, valor)
+  infoGuerreiro()
