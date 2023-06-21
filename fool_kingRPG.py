c33b49b3eb1263c99932efffa404fa2e552df34e
Lucas Felipe <devlucasfelipe@gmail.com>
2023-06-21 20:31:45 -0300

    GuerreiroGameplay_v3

--- fool_kingRPG.py
+++ fool_kingRPG.py
@@ -214,4 +214,89 @@ elif escolhaDoPersonagem == 'II':
     pulaLinha()
 
     bugMode()
-    print('quer')
+    print('aceita')
+
+  else:
+    sleep(7)
+    print("""Oh! Você está aí!""")
+    sleep(7)
+    print("""Que inesperado, achei que mais uma vez você iria fugir de seus problemas.""")
+    sleep(5)
+    print("""Problemas estes que nem são tão grandes assim, se forem comparados aos de outras pessoas. Vamos dizer que é só uma fuga de sua personalizade fútil, ou algo semelhante.""")
+    sleep(10)
+    pulaLinha()
+    print('...')
+    sleep(10)
+    print('Ei...')
+    sleep(5)
+    print("""Qual é o seu maior sonho? O que você almeja?""")
+    sleep(7)
+    print("""Quem são as pessoas que você realmente ama? Você gosta do que faz?""")
+    sleep(7)
+    print("""E a sua rotina?""")
+    sleep(10)
+    pulaLinha()
+    print('...')
+    pulaLinha()
+    def todasAsPerguntas():
+      perguntas = [
+      ('Já pagou seus impostos?'),
+      ('Já fez suas obrigações?'),
+      ('Por que tanta revolta?'),
+      ('Você me odeia?'),
+      ('Porque tanto ódio?'),
+      ('Por que você é assim?'),
+      ('As pessoas vão te achar estranho, não vão?'),
+      ('Você acha mesmo que comprar algumas coisinhas vão te tirar desse poço?'),
+      ('Sua felicidade é momentânea, não acha?'),
+      ('Você acha mesmo que alguém te entende?'),
+      ('Você acredita mesmo que vai conquistar seus sonhos?'),
+      ('E todas as suas paixões?'),
+      ('Você ainda acha que é amado pelas pessoas?'),
+      ('É serio que você ainda se acha importante para as pessoas? Puff...'),
+      ('Você não cansa de se apaixonar, não é? Acha que isso vai te livrar dos sufocos? É uma maneira de se esconder'),
+      ('Você não se encaixa em nenhum padrão da sociedade, ainda não percebeu?'),
+  ]
+
+      def IterarAsPerguntas():
+        for pergunta in perguntas:
+          print(pergunta)
+          sleep(1)
+    
+    
+    IterarAsPerguntas()
+
+
+  todasAsPerguntas()
+  pulaLinha()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
